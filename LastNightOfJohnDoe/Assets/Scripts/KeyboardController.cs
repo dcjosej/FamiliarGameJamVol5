fc68298a1a17d621f828d5847943d387acdb00dc
Jose J. Delgado <dcjosej@gmail.com>
2015-10-25 21:53:40 +0100

    0897897

---
+++ LastNightOfJohnDoe/Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour
+{
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("Hall");
+		}
+	}
+}
---
+++ LastNightOfJohnDoe/Assets/Scripts/KeyboardMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardMenu : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("Controles");
+		}
+	}
+}
